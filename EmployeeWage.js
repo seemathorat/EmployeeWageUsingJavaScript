13e7c21b8622ecea9ac006f09f2cfa3b7435cbb7
seema thorat <seemagade97@gmail.com>
2021-04-24 10:28:10 +0530

    Store the Daily Wage along with Total Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -76,3 +76,30 @@ console.log("Hour: " +empHrs +  "Emp wage" + empWage);
     let empWage = totalEmpHrs * WAGE_PER_HOUR;
     console.log("UC-5 Total Days: " +totalWorkingDays + " Total Hrs :" + totalEmpHrs + " Emp Wage :" + empWage);
 }
+
+const MAX_HRS_IN_MONTH = 160;
+//const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+let empDailyWageMap = new Map();
+let empDailyHrsMap = new Map();
+
+function calcDailyWage(empHrs)
+{
+   return empHrs * WAGE_PER_HOUR; 
+}
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+{
+   totalWorkingDays++;
+   let empCheck = Math.floor(Math.random() *10) % 3;
+   let empHrs = getWorkingHours(empCheck);
+   totalEmpHrs += empHrs;
+empDailyWageArr.push(calcDailyWage(empHrs));
+empDailyHrsMap.set(totalWorkingDays, empHrs);
+empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+}
+
+let totalEmpWage = calcDailyWage(totalEmpHrs);
+console.log("UC-6 Total Days: " + totalWorkingDays + "Total Hrs: " + totalEmpHrs + "Emp Wage: " + totalEmpWage);
